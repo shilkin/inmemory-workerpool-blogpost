9f008f5b555f96ce18250f8d490e71d022d37e39
igor <vivaldi7772@gmail.com>
2024-11-12 20:01:21 +0300

    lab-3

--- example/goroutine-leak/cmd/webserver/internal/service/user_test.go
+++ example/goroutine-leak/cmd/webserver/internal/service/user_test.go
@@ -10,88 +10,80 @@ import (
 	"github.com/pkg/errors"
 	"github.com/shilkin/inmemory-workerpool-blogpost/example/goroutine-leak/cmd/webserver/internal/service"
 	mock "github.com/shilkin/inmemory-workerpool-blogpost/example/goroutine-leak/cmd/webserver/internal/service/internal/mock"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
+type TestUserCreateSuite struct {
+	suite.Suite
+	repoMock      *mock.MockUserRepository
+	analyticsMock *mock.MockAnalytics
+	pooMock       *mock.MockPool
+}
+
+func (s *TestUserCreateSuite) SetupTest() {
+	s.T().Parallel()
+	ctrl := gomock.NewController(s.T())
+	s.T().Cleanup(ctrl.Finish)
+
+	s.repoMock = mock.NewMockUserRepository(ctrl)
+	s.analyticsMock = mock.NewMockAnalytics(ctrl)
+	s.pooMock = mock.NewMockPool(ctrl)
+}
+
+func (s *TestUserCreateSuite) TestUserCreateSuccessWithPool() {
+	var isAnalyticsCalled atomic.Bool
+
+	s.repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("id", nil)
+	s.analyticsMock.EXPECT().Send(gomock.Any(), "user created", "id").
+		Do(func(_ context.Context, _, _ string) {
+			isAnalyticsCalled.Store(true)
+		})
+
+	userService := service.NewUserService(s.repoMock, s.analyticsMock, s.pooMock)
+
+	err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
+	s.Require().NoError(err)
+	s.Require().Eventually(func() bool {
+		return isAnalyticsCalled.Load()
+	}, time.Second, time.Millisecond)
+}
+
+func (s *TestUserCreateSuite) TestUserCreateSuccessfully() {
+	s.repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("id", nil)
+	s.pooMock.EXPECT().Enqueue(context.Background(), gomock.Any()).Return(nil)
+
+	userService := service.NewUserService(s.repoMock, s.analyticsMock, s.pooMock)
+
+	err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
+	s.Require().NoError(err)
+}
+
+func (s *TestUserCreateSuite) TestUnableToSaveUserInTheRepo() {
+	s.repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("1", errors.New("Except"))
+
+	userService := service.NewUserService(s.repoMock, s.analyticsMock, s.pooMock)
+
+	err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
+
+	s.Require().Error(err)
+	s.Require().Contains(s.T(), err, "Except")
+}
+
+func (s *TestUserCreateSuite) TestUnableToSendAnalytics() {
+	gomock.InOrder(
+		s.repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("1", nil),
+		s.pooMock.EXPECT().Enqueue(context.Background(), gomock.Any()).Return(errors.New("Enqueue Except")),
+	)
+
+	userService := service.NewUserService(s.repoMock, s.analyticsMock, s.pooMock)
+
+	err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
+
+	s.Require().Error(err)
+	s.Require().Contains(s.T(), err, "Enqueue Except")
+
+}
+
 func TestUserCreate(t *testing.T) {
-	t.Parallel()
-
-	ctrl := gomock.NewController(t)
-	t.Cleanup(ctrl.Finish) // defer ctrl.Finish() <- linters warning
-
-	t.Run("user created sucessfully with real pool", func(t *testing.T) {
-		t.Parallel()
-
-		repoMock := mock.NewMockUserRepository(ctrl)
-		analyticsMock := mock.NewMockAnalytics(ctrl)
-		pool := service.NewPool(1) // real pool
-
-		var isAnalyticsCalled atomic.Bool
-
-		repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("id", nil)
-		analyticsMock.EXPECT().Send(gomock.Any(), "user created", "id").
-			Do(func(_ context.Context, _, _ string) {
-				isAnalyticsCalled.Store(true)
-			})
-
-		userService := service.NewUserService(repoMock, analyticsMock, pool)
-
-		err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
-		require.NoError(t, err) // fail now
-
-		// wait for analytics being called or fail the test
-		require.Eventually(t, isAnalyticsCalled.Load, time.Second, time.Millisecond)
-	})
-	t.Run("user created sucessfully", func(t *testing.T) {
-		t.Parallel()
-
-		repoMock := mock.NewMockUserRepository(ctrl)
-		analyticsMock := mock.NewMockAnalytics(ctrl)
-		pooMock := mock.NewMockPool(ctrl)
-
-		repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("id", nil)
-		pooMock.EXPECT().Enqueue(context.Background(), gomock.Any()).Return(nil)
-
-		userService := service.NewUserService(repoMock, analyticsMock, pooMock)
-
-		err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
-		require.NoError(t, err) // fail now
-	})
-
-	t.Run("unable to save user in the repo", func(t *testing.T) {
-		t.Parallel()
-
-		repoMock := mock.NewMockUserRepository(ctrl)
-		analyticsMock := mock.NewMockAnalytics(ctrl)
-		pooMock := mock.NewMockPool(ctrl)
-
-		repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("1", errors.New("Except"))
-
-		userService := service.NewUserService(repoMock, analyticsMock, pooMock)
-
-		err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
-
-		require.Error(t, err) // fail now
-		require.Contains(t, err.Error(), "Except")
-	})
-
-	t.Run("unable to enqueue job", func(t *testing.T) {
-		t.Parallel()
-
-		repoMock := mock.NewMockUserRepository(ctrl)
-		analyticsMock := mock.NewMockAnalytics(ctrl)
-		pooMock := mock.NewMockPool(ctrl)
-
-		gomock.InOrder(
-			repoMock.EXPECT().Create(context.Background(), "Jon Doe", "jon.doe@example.com").Return("1", nil),
-			pooMock.EXPECT().Enqueue(context.Background(), gomock.Any()).Return(errors.New("Enqueue Except")),
-		)
-
-		userService := service.NewUserService(repoMock, analyticsMock, pooMock)
-
-		err := userService.Create(context.Background(), "Jon Doe", "jon.doe@example.com")
-
-		require.Error(t, err) // fail now
-		require.Contains(t, err.Error(), "Enqueue Except")
-	})
+	suite.Run(t, new(TestUserCreateSuite))
 }
